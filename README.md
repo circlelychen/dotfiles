7873c35cae83dc39328ab264ad7a6a4176e1f6aa
circlelychen <truecirclely@gmail.com>
2020-09-23 18:53:34 +0800

    add homebrew install

--- README.md
+++ README.md
@@ -3,5 +3,10 @@ A repo containing dotfiles
 
 ## Install
 ```bash
+$> install
+```
+
+## Setup
+```bash
 $> setup.sh
 ```
---
+++ homebrew/install.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+#
+# Homebrew
+#
+# This installs some of the common dependencies needed (or at least desired)
+# using Homebrew.
+
+# Check for Homebrew
+if test ! $(which brew)
+then
+  echo "  Installing Homebrew for you."
+
+  # Install the correct homebrew for each OS type
+  if test "$(uname)" = "Darwin"
+  then
+    # Install Homebrew
+    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+    # Install packages
+    apps=(
+        ag
+        bash-completion2
+        cmake
+        git
+        grep
+        httpie
+        jq
+        wget
+    )
+    brew install "${apps[@]}"
+
+    # Git comes with diff-highlight, but isn't in the PATH
+    #ln -sf "$(brew --prefix)/share/git-core/contrib/diff-highlight/diff-highlight" /usr/local/bin/diff-highlight
+  fi
+
+fi
+
+exit 0
